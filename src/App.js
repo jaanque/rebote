1767ecc90898f20d31245c0e8bd1ba4801c9e9ae
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-06-26 15:13:22 +0200

    ADD // banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imageUrl = "https://i.ibb.co/C3w5XRr9/bg-img.webp";
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div 
+        className="App-container"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      >
+        <h1 className="title">Own the moment</h1>
+      </div>
     </div>
   );
 }
